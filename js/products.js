6e73c120c11feb410c14d4fc95631914cbd2753d
dev-fstack <68856092+dev-fstack@users.noreply.github.com>
2020-08-06 18:55:08 -0300

    Fetch
    Se agrega el resultado del Fetch al HTML

--- js/products.js
+++ js/products.js
@@ -1,6 +1,47 @@
+var categoriesArray = [];
+
+function showCategoriesList(array){
+
+    let htmlContentToAppend = "";
+    for(let i = 0; i < array.length; i++){
+        let category = array[i];
+
+        htmlContentToAppend += `
+        <div class="list-group-item list-group-item-action">
+            <div class="row">
+                <div class="col-3">
+                    <img src="` + category.imgSrc + `" alt="` + category.description + `" class="img-thumbnail">
+                </div>
+                <div class="col">
+                    <div class="d-flex w-100 justify-content-between">
+                        <h4 class="mb-1">`+ category.name +`</h4>
+                        <small class="text-muted">` + category.soldCount + ` artículos</small>
+                        <small class="text-muted">` + category.cost + category.currency +` </small>
+                    </div>
+                    <div>
+                        <small class="text-muted">` + category.description + `</small>
+                    </div>
+                </div>
+            </div>
+        </div>
+        `
+
+        document.getElementById("container p-5").innerHTML = htmlContentToAppend;
+    }
+}
+
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function (e) {
-
+showSpinner();
+document.addEventListener("DOMContentLoaded", function(e){
+    getJSONData("https://japdevdep.github.io/ecommerce-api/product/all.json").then(function(resultObj){
+        if (resultObj.status === "ok")
+        {
+            categoriesArray = resultObj.data;
+            //Muestro las categorías ordenadas
+            showCategoriesList(categoriesArray);
+        }
+        hideSpinner();
+    });
 });
