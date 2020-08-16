492bccea40739d665da6cc20764cfcfb8fbf764f
dev-fstack <68856092+dev-fstack@users.noreply.github.com>
2020-08-16 18:23:46 -0300

    Login & Products
    Se hizo el login, la lista de productos y se cambio el index al login.

--- js/login.js
+++ js/login.js
@@ -1,6 +1,34 @@
-//Función que se ejecuta una vez que se haya lanzado el evento de
-//que el documento se encuentra cargado, es decir, se encuentran todos los
-//elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
+const form = document.getElementsByTagName("form")[0];
+const email = document.getElementById("inputEmail");
+const button = document.getElementsByTagName("button")[0];
+const password = document.getElementById("inputPassword");
 
+
+email.addEventListener("input", function (event) {
+  if (email.validity.typeMismatch) {
+    email.setCustomValidity("I am expecting an e-mail address!");
+  } else {
+    email.setCustomValidity("");
+  }
 });
+
+password.addEventListener("input", function(event){
+    if (password.validity.tooLong || password.validity.tooShort) {
+        password.setCustomValidity("8-20 numbers please");
+    } else {
+        password.setCustomValidity("");
+    }
+})
+
+form.addEventListener("submit", function (event) {
+    if(email.validity.valid && password.validity.valid) {
+        event.preventDefault();
+    }
+});
+
+button.addEventListener("click", function(event) {
+    if(email.validity.valid && password.validity.valid) {
+        window.location.replace("inicio.html");
+    }
+});
+
